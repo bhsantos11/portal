1d11292c031613dcf1892345829a6abc36218bc0
bhsantos <bhauersantos@gmail.com>
2019-07-15 21:26:41 -0300

    first ajax

--- upload.js
+++ upload.js
@@ -1,8 +1,20 @@
 
 function uploadFile(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "URL")
+
     var fileNameEl = document.getElementById("file-name")
     var fileCompleteEl = document.getElementById("file-complete")
 
     console.log("File Name" + fileNameEl.value);
     console.log("File" + fileCompleteEl.value);
+
+    xhttp.responseText = fileNameEl.value
+    xhttp.responseText += fileCompleteEl.value
+
+
+
+
+    
+    xhttp.send()
 }
